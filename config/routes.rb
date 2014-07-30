fc7144daebba93bc96f4cff2063ecc8288e4ffb9
Shane Wierenga <shane@midwestsignco.com>
2014-07-30 18:01:28 -0400

    added contacts controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   
   get 'timerecords/timeout/:id' => 'timerecords#timeout'
 
+  resources :contacts
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
