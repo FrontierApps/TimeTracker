9ffb50a8084fe90842576a0f0bd686c707d17654
Shane Wierenga <shane@midwestsignco.com>
2014-08-11 15:19:42 -0400

    changes to employee view

--- app/models/timerecord.rb
+++ app/models/timerecord.rb
@@ -2,7 +2,7 @@ class Timerecord < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :task
 	d=Date.today
-	d=d.at_beginning_of_week
+	d=d.at_beginning_of_week-1.day
 	scope :thisweek, -> {where('timein > ?', d).all} 
 	scope :notimeout, -> {where('timeout IS ?', nil)} 
 	scope :timethisweek, -> {where('timein > ?', d).all} 
