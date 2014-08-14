36fbe64b35b0157a7e0e5fc194ff8a39e080445b
Shane Wierenga <shane@midwestsignco.com>
2014-08-13 22:08:37 -0400

    change timerecord.rb

--- app/models/timerecord.rb
+++ app/models/timerecord.rb
@@ -2,10 +2,13 @@ class Timerecord < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :task
 	d=Date.today
+	d=Date.today
 	d=d.at_beginning_of_week-1.day
-	scope :thisweek, -> {where('timein > ?', d).all} 
+	d1=d.at_beginning_of_week-7.day
 	scope :notimeout, -> {where('timeout IS ?', nil)} 
 	scope :timethisweek, -> {where('timein > ?', d).all} 
+	scope :weekstart, -> {where('timein >= ?', d1).all} 
+	scope :weekend, -> {where('timein <= ?', d).all} 
 	scope :currentuser, ->(user) {where('user_id = ?', user )} 
 	scope :selecteduser, ->(user) {where('user_id = ?', user )} 
 	
