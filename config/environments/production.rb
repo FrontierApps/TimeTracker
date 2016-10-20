b1de312e4abd6ac2ccde46182181acfb34e1539f
Shane Wierenga <shane@midwestsignco.com>
2016-10-20 10:51:53 -0400

    fixed host in production.rb again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'midwestsignco.com' }
+  config.action_mailer.default_url_options = { :host => 'tracker.midwestsignco.com' }
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
